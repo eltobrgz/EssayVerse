cfedbab0b4489d4cf7dd63b0536701572aca369a
Elto <eltobrgz.code@gmail.com>
2025-07-02 19:51:22 +0000

    ainda ta com o mesmo problema, sera que seria legal em vez de ler o cook

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -19,11 +19,19 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
+          // If the cookie is updated, update the cookies for the request and response
           request.cookies.set({ name, value, ...options });
+          response = NextResponse.next({
+            request: { headers: request.headers },
+          });
           response.cookies.set({ name, value, ...options });
         },
         remove(name: string, options: CookieOptions) {
+          // If the cookie is removed, update the cookies for the request and response
           request.cookies.set({ name, value: '', ...options });
+          response = NextResponse.next({
+            request: { headers: request.headers },
+          });
           response.cookies.set({ name, value: '', ...options });
         },
       },
@@ -31,7 +39,10 @@ export async function updateSession(request: NextRequest) {
   );
 
   // This will refresh the session cookie if it's expired.
+  // It might also trigger the `set` or `remove` methods above, which re-assigns
+  // the `response` variable.
   const { data: { user } } = await supabase.auth.getUser();
 
+  // The `response` variable is now guaranteed to have the latest cookies.
   return { response, user };
 }
